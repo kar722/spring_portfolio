d7e90a6824ac9ce277e5e784af419fcdf89cbd8d
kar722 <karthikv722@gmail.com>
2022-11-17 21:38:46 -0800

    APCalendar FRQ

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calendar/APCalendar.java
@@ -0,0 +1,99 @@
+package com.nighthawk.spring_portfolio.mvc.calendar;
+import java.util.*;
+import java.time.YearMonth;
+// Prototype Implementation
+
+public class APCalendar {
+
+    /** Returns true if year is a leap year and false otherwise.
+     * isLeapYear(2019) returns False
+     * isLeapYear(2016) returns True
+     */          
+    public static boolean isLeapYear(int year) {
+        // Yes if year divisible by 4, no if by 100, but also yes if 400.
+        if ((year % 4 == 0) && (year % 400 == 0 || !(year % 100 == 0))) {
+            return true;
+        }
+        else {
+            return false;
+        }
+        }
+        
+    /** Returns the value representing the day of the week 
+     * 0 denotes Sunday, 
+     * 1 denotes Monday, ..., 
+     * 6 denotes Saturday. 
+     * firstDayOfYear(2019) returns 2 for Tuesday.
+    */
+    public static int firstDayOfYear(int year) {
+        // Gets date object of 1/1/year, date object returns day value for the first day of the year
+        Date date = new Date(year, 0, 0);
+        return date.getDay();
+        }
+
+
+    /** Returns n, where month, day, and year specify the nth day of the year.
+     * This method accounts for whether year is a leap year. 
+     * dayOfYear(1, 1, 2019) return 1
+     * dayOfYear(3, 1, 2017) returns 60, since 2017 is not a leap year
+     * dayOfYear(3, 1, 2016) returns 61, since 2016 is a leap year. 
+    */ 
+    public static int dayOfYear(int month, int day, int year) {
+        // initializes dayValue as 0
+        int dayVal = 0;
+        for (int i = 1; i < month; i++) {
+            // Creates a yearmonth object for each month in the year
+            int monthDays = YearMonth.of(year, i).lengthOfMonth();
+            dayVal += monthDays;
+        }
+        //Adds the day of the incomplete month to dayVal
+        dayVal += day;
+        return dayVal;
+        }
+
+    /** Returns the number of leap years between year1 and year2, inclusive.
+     * Precondition: 0 <= year1 <= year2
+    */ 
+    public static int numberOfLeapYears(int year1, int year2) {
+        // initializes leapYearCount as 0
+        int leapYearCount = 0;
+
+        // iterates through every year to find if it is a leap year
+        for (int i = year1; i <= year2; i++) {
+            if (isLeapYear(i)) {
+                leapYearCount++;
+            }
+        }
+
+        return leapYearCount;
+        }
+
+    /** Returns the value representing the day of the week for the given date
+     * Precondition: The date represented by month, day, year is a valid date.
+    */
+    public static int dayOfWeek(int month, int day, int year) { 
+        //initializes date object, gets day value of that week of initialized date
+        Date date = new Date(year, month - 1, day);
+        // fix for date.getDay being inacurate to the format we originally had, requires if statement
+        if (date.getDay() == 0) {
+            return 6;
+        }
+        else {
+            return date.getDay() - 1;
+        }
+        }
+
+
+    /** Tester method */
+    public static void main(String[] args) {
+        // Private access modifiers
+        System.out.println("firstDayOfYear: " + APCalendar.firstDayOfYear(2022));
+        System.out.println("dayOfYear: " + APCalendar.dayOfYear(1, 1, 2022));
+
+        // Public access modifiers
+        System.out.println("isLeapYear: " + APCalendar.isLeapYear(2022));
+        System.out.println("numberOfLeapYears: " + APCalendar.numberOfLeapYears(2000, 2022));
+        System.out.println("dayOfWeek: " + APCalendar.dayOfWeek(1, 1, 2022));
+    }
+
+}
